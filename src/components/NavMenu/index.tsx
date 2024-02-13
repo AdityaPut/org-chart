ab01c2d4ad38e86e12b3d4ca1c771bb0e5f2eea4
Aditya Putra <adityaaputraaa98@gmail.com>
2024-02-13 22:58:55 +0700

    feat: add NavMenu component and update layout.tsx

---
+++ src/components/NavMenu/index.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export const NavMenu = () => {
+  return (
+    <nav className='bg-white border-gray-200 dark:bg-gray-900 shadow-md fixed w-[100vw] z-10'>
+      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        <a href='#' className='flex items-center space-x-3 rtl:space-x-reverse'>
+          <span className='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
+            structureOrgChart
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
