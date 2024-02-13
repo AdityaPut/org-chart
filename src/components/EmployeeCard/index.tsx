bd518dba4f73ead549df8dad7e9db54bc9e11722
Aditya Putra <adityaaputraaa98@gmail.com>
2024-02-14 00:30:50 +0700

    feat: add node line to improve the styling

--- src/components/EmployeeCard/index.tsx
+++ src/components/EmployeeCard/index.tsx
@@ -1,26 +1,27 @@
 'use client';
 
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/Avatar';
 import {
   Card,
   CardContent,
   CardDescription,
-  CardHeader,
-  CardTitle,
+  CardTitle
 } from '@/components/ui/Card';
-import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/Avatar';
-import type { Employee } from '@/types';
 import { useGetInitialName } from '@/hooks/useGetInitialName';
+import type { Employee } from '@/types';
+import { cn } from '@lib/utils';
 
 type EmployeeCardProps = {
   employee: Employee;
+  className?: string;
 };
 
 const EmployeeCard = (props: EmployeeCardProps) => {
-  const { employee } = props;
+  const { employee, className } = props;
   const { name, avatar, role } = employee;
   const initialName = useGetInitialName(name);
   return (
-    <Card className='w-44 h-[185px]'>
+    <Card className={cn('w-44 h-[185px]', className)}>
       <CardContent className='flex flex-col justify-center items-center gap-1 p-2'>
         {avatar && (
           <Avatar className='w-[100px] h-[100px] my-2'>
--- src/components/OrganizationNode/index.tsx
+++ src/components/OrganizationNode/index.tsx
@@ -2,31 +2,85 @@
 
 import { Employee } from '@/types';
 import EmployeeCard from '../EmployeeCard';
+import { isEmpty, cn, isArray, isUndefined } from '@lib/utils';
+import type { ReactNode } from 'react';
 
 type OrganizationNodeProps = {
   employee: Employee;
+  index?: number;
+  totalSiblings?: number;
+};
+
+const ItemWithNodeLine = (
+  props: OrganizationNodeProps & { children: ReactNode }
+) => {
+  const { employee, children, index, totalSiblings = 1 } = props;
+
+  const getClassNames = () => {
+    if (totalSiblings === 1 || isUndefined(index)) {
+      return 'border-t-0';
+    }
+    // if the index and the totalSiblings more than 1, the line will be on the right side of the card
+    if (index === 0 && totalSiblings > 1) {
+      return 'self-end border-r-0 border-l-2';
+    }
+    // if the index is the last one, the line will be on the left side of the card
+    if (index === totalSiblings - 1) {
+      return 'self-start border-l-0 border-r-2';
+    }
+    // otherwise, the line will be full on the top of the card
+    return 'border-t-2 w-full border-r-0 ';
+  };
+  return (
+    <div className='flex flex-col items-center '>
+      {!isEmpty(employee.managerId) && (
+        <div
+          className={cn(
+            'h-5 w-[50%] border-gray-300 border-r-2 border-t-2 self-start',
+            getClassNames()
+          )}
+        />
+      )}
+      {children}
+    </div>
+  );
 };
 
 export const OrganizationNode = (props: OrganizationNodeProps) => {
   const { employee } = props;
 
-  if (employee.children) {
+  const renderEmployeeCard = () => (
+    <ItemWithNodeLine {...props}>
+      <EmployeeCard employee={employee} className='mx-1.5' />
+    </ItemWithNodeLine>
+  );
+
+  if (isArray(employee?.children)) {
     return (
-      <div className='flex flex-col items-center'>
-        <EmployeeCard employee={employee} />
+      <div>
+        {renderEmployeeCard()}
+        {/* The second node line is always located in the middle of the card */}
+        <div className='h-5 w-[50%] self-start border-gray-300 border-r-2' />
         <div
-          className={'grid gap-4 mt-8'}
+          className={'grid '}
           style={{
             gridTemplateColumns: `repeat(${employee.children.length}, 1fr)`,
           }}
         >
-          {employee.children.map((employee) => (
-            <OrganizationNode key={employee.id} employee={employee} />
-          ))}
+          {employee.children.map((emp, index) => {
+            return (
+              <OrganizationNode
+                key={emp.id}
+                employee={emp}
+                index={index}
+                totalSiblings={employee.children?.length}
+              />
+            );
+          })}
         </div>
       </div>
     );
   }
 
-  return <EmployeeCard employee={employee} />;
+  return renderEmployeeCard();
 };
