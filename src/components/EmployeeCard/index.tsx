4c75ebe9dc1c8c178c55da2e330e569efe85acff
Aditya Putra <adityaaputraaa98@gmail.com>
2024-02-14 21:11:51 +0700

    feat: improve EmployeeCard styling

--- src/components/EmployeeCard/index.tsx
+++ src/components/EmployeeCard/index.tsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/Avatar';
+import { Avatar, AvatarFallback, AvatarImage } from '@components/ui/Avatar';
 import {
   Card,
   CardContent,
   CardDescription,
-  CardTitle
-} from '@/components/ui/Card';
+  CardTitle,
+} from '@components/ui/Card';
 import { useGetInitialName } from '@/hooks/useGetInitialName';
 import type { Employee } from '@/types';
 import { cn } from '@lib/utils';
@@ -24,12 +24,14 @@ const EmployeeCard = (props: EmployeeCardProps) => {
     <Card className={cn('w-44 h-[185px]', className)}>
       <CardContent className='flex flex-col justify-center items-center gap-1 p-2'>
         {avatar && (
-          <Avatar className='w-[100px] h-[100px] my-2'>
-            <AvatarImage src={avatar} alt={name} />
-            <AvatarFallback>{initialName}</AvatarFallback>
-          </Avatar>
+          <div className='relative'>
+            <Avatar className='w-[100px] h-[100px] my-2'>
+              <AvatarImage src={avatar} alt={name} />
+              <AvatarFallback className='text-2xl'>{initialName}</AvatarFallback>
+            </Avatar>
+          </div>
         )}
-        <CardTitle>{name}</CardTitle>
+        <CardTitle className='capitalize'>{name}</CardTitle>
         <CardDescription>{role}</CardDescription>
       </CardContent>
     </Card>
